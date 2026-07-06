3f153422babf5fcfe2ac5ba9a19b8caba784d024
Domenico DiNicola <dom.dinicola@gmail.com>
2026-07-06 13:25:13 +0200

    Revert "Fix post-filter search: return raw API total and clear scan cache between tests"
    This reverts commit f4b06bb091f595a1142b2af4bdc53bae35c87429.

--- tests/test_graph_client.py
+++ tests/test_graph_client.py
@@ -4,12 +4,7 @@ import pytest
 import requests
 
 from sharepoint_rest_api.builders.rest_builder import RestBuilder
-from sharepoint_rest_api.graph_client import GraphClient, GraphClientError, GRAPH_URL, _scan_cache
-
-
-@pytest.fixture(autouse=True)
-def _clear_scan_cache():
-    _scan_cache.clear()
+from sharepoint_rest_api.graph_client import GraphClient, GraphClientError, GRAPH_URL
 
 
 # ---- __init__ ----------------------------------------------------------------
